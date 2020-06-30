db4bfe5a708f807c620ea61b39095ae8164680b4
CodenificenT <xristianty@gmail.com>
2020-06-29 20:34:07 -0400

    closed

--- derekbanasYT/arrays.cpp
+++ derekbanasYT/arrays.cpp
@@ -3,13 +3,26 @@
 
 using namespace std;
 
-int arrays()
+int main()
 {
-    int myArray[5];
-
     int badNums[5] = {4, 13, 14, 24, 34};
 
-    export array
+    cout << "The second bad number is " << badNums[1] << endl;
 
-    return(myArray);
+    char myName[9][6] = {{'C', 'h', 'r', 'i', 's', 't', 'i', 'a', 'n'},
+                                       {'L', 'e', 'g', 'e', 'n', 'd'}};
+
+     cout << "2nd Letter in 2nd array: "  << myName[1][1] << endl;
+
+          //myName[0][0] = ' ';
+          myName[0][1] = 'K';
+
+    for (int j = 0; j < 10; j++) {
+        for  (int k = 0; k < 7; k++) {
+            cout << myName[j][k];
+        }
+    }
+
+
+    return(0);
 }
---
+++ dereknewtut/newcpp.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    cout << "Hello to my bestie!" << endl;
+
+    return(0);
+}
